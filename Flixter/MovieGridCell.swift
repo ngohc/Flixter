7e237e66e799e22ee4a0a9b447ea65fbf4a17110
Chantel <ngohchantel@hotmail.com>
2021-02-25 08:56:56 +0800

    Updated with Part 2 - grid view + movie details page

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Chantel Ngoh on 24/2/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
